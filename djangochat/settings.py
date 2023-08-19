edfc52d85417f83af09f8774ee2643d9bd7b4b4c
Devanshu Gupta <devanshu.kissu.0000@gmail.com>
2023-08-19 20:12:53 +0530

    for host

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fa-ddn%8p=rghoq!2h*xrufphq@xx53097df3+jn6jihtj5kpn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
